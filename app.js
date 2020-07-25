a490a09d876ecfe3889ce2395207a79ddc25db9d
karannegi20 <karan.negi20@gmail.com>
2020-07-25 17:41:52 +0530

    Add and remove note functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes');
+const notes = require('./notes');
 
 yargs.version('1.1.0');
 
@@ -21,16 +21,22 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title : ' + argv.title);
-    console.log('Body : ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function () {
-    console.log('Removing note');
+  builder: {
+    title: {
+      describe: 'title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
--- notes.js
+++ notes.js
@@ -1,5 +1,63 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
 const getNotes = function () {
   return 'Your notes....';
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log('New note added');
+  } else {
+    console.log('Title already taken!');
+  }
+};
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+
+  const newNotes = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (newNotes.length !== notes.length) {
+    console.log(chalk.bgGreen('Note Removed!'));
+  } else {
+    console.log(chalk.bgRed('No Note found!'));
+  }
+
+  saveNotes(newNotes);
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+};
